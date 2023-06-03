c7e7ff21a2627db26fa7304056c2095902ec60b1
excel1an <103880075+excel1an@users.noreply.github.com>
2023-06-03 14:59:31 -0700

    Update header.php

--- header.php
+++ header.php
@@ -2,52 +2,54 @@
 			<div id="logo">
 				<a href="index.php"> <!-- Lien autour de l'image du logo -->
 					<img src="img/logo grand.png" alt="Logo">
-					<a class="link" href="compte.php">
-						
-					</a>
+					
 				</a>
+			</div>
+			<div id="search-bar">
+				<input type="text" placeholder="Rechercher...">
+				<button type="submit" style="position: right;"> <img src="img/jobece.png" style="width: 20px; height: 20px;"> </button>
 			</div>
 			<div id="tabs">
 				<a href="index.php">
 					
-						<img src="img/maison.png" style="width: 25px; height: 25px;margin-right: 10px">
+						<img src="img/maison.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Accueil
-					<img src="img/maison.png" style="width: 25px; height: 25px;margin-left: 10px">
+					<img src="img/maisonece.png" style="width: 50px; height: 50px;margin-left: 10px">
 				</a>
 				<a href="monreseau.php">
 					
-						<img src="img/reseau.png" style="width: 25px; height: 25px;margin-right: 10px">
+						<img src="img/reseau.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Mon Réseau
-					<img src="img/reseau.png" style="width: 25px; height: 25px;margin-left: 10px">
+					<img src="img/reseauece.png" style="width: 50px; height: 50px;margin-left: 10px">
 				</a>
 				<a href="notifications.php">
 					
-						<img src="img/notif.png" style="width: 25px; height: 25px;margin-right: 10px">
+						<img src="img/notif.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Notifications
-					<img src="img/notif.png" style="width: 25px; height: 25px;margin-left: 10px">
+					<img src="img/notifece.png" style="width: 50px; height: 50px;margin-left: 10px">
 				</a>
 				<a href="messagerie.php">
 					
-						<img src="img/chat.png" style="width: 25px; height: 25px;margin-right: 10px">
+						<img src="img/chat.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Messagerie
-					<img src="img/chat.png" style="width: 25px; height: 25px;margin-left: 10px">
+					<img src="img/chatece.png" style="width: 50px; height: 50px;margin-left: 10px">
 				</a>
 				<a href="emplois.php">
 					
-						<img src="img/job.png" style="width: 25px; height: 25px;margin-right: 10px">
+						<img src="img/job.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Emplois
-					<img src="img/job.png" style="width: 25px; height: 25px;margin-left: 10px">
+					<img src="img/jobece.png" style="width: 50px; height: 50px;margin-left: 10px">
 				</a>
 				<a href="compte.php">
-					<img src="img/profil.png" style="width: 25px; height: 25px;margin-right: 10px">
+					<img src="img/profil.png" style="width: 50px; height: 50px;margin-right: 10px">
 					
 					Mon Profil
-					<img :hover src="img/maisonece.png" style="width: 25px; height: 25px; margin-left:10px">
+					<img src="img/profilece.png" style="width: 50px; height: 50px; margin-left:10px">
 
 				</a>
 			</div>
