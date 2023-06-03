fd6cd7b7162edf01deac2be97e6c2d39cf72233b
excel1an <103880075+excel1an@users.noreply.github.com>
2023-06-03 15:08:49 -0700

    Update monreseau.php

--- monreseau.php
+++ monreseau.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8"/>
 		<title>Mon Réseau</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
 		<?php include "header.php";?>
 
 		<div class="header">
 			<div class="container2">
-				<a class="button-small" href="Accueil.html">Retour</a><br>
+				<a class="button-small" href="index.php">Retour</a><br>
 			</div>
 			<h1>Mon Réseau</h1>
 		</div>
@@ -23,8 +22,11 @@
 		<div id="footer">
 			<div style="display: flex; align-items: center;">
 				<img src="img/logo grand.png">
-				<span style="text-align: center;">© 2023 - ECE in - <a href="https://www.google.com/maps/place/37+Quai+de+Grenelle,+75015+Paris/@48.8515004,2.2872324,17z/data=!3m1!4b1!4m6!3m5!1s0x47e6700497ee3ec5:0xdd60f514adcdb346!8m2!3d48.8515004!4d2.2872324!16s%2Fg%2F11bw3y1mf8?entry=ttu" target="_blank">37 Quai de Grenelle 75015 Paris</a>
-					- Célian Guillaume / Julien Smatt</span>
+				<span style="text-align: center;">Tous droits réservés © 2023 - ECE in 
+				<br>
+				Contact :   
+				<a href="https://www.google.com/maps/place/37+Quai+de+Grenelle,+75015+Paris/@48.8515004,2.2872324,17z/data=!3m1!4b1!4m6!3m5!1s0x47e6700497ee3ec5:0xdd60f514adcdb346!8m2!3d48.8515004!4d2.2872324!16s%2Fg%2F11bw3y1mf8?entry=ttu" target="_blank" style="text-decoration: none;">   37 Quai de Grenelle 75015 Paris</a> - +33 6 79 22 99 79 <br>   
+ celian.guillaume@edu.ece.fr - julien.smatt@edu.ece.fr -  Célian Guillaume & Julien Smatt</span>
 			</div>
 		</div>
 	</body>
