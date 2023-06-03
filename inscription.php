bcbd4cc2a71220d3962660822a48f6dec7a59b82
excel1an <103880075+excel1an@users.noreply.github.com>
2023-06-03 15:05:52 -0700

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,10 +16,63 @@
   </script>
   <link rel="stylesheet" href="styledebut.css">
 </head>
+
+<?php
+if (isset($_POST['button1'])) {
+    if (!empty($_POST['password'])) {
+        include 'database.php';
+        
+        $c = $database->prepare("SELECT * FROM utilisateur WHERE username = ?");
+        $c->bind_param("s", $_POST['username']);
+        $c->execute();
+        $result = $c->get_result();
+
+        $c1 = $database->prepare("SELECT * FROM utilisateur WHERE mail = ?");
+        $c1->bind_param("s", $_POST['email']);
+        $c1->execute();
+        $result1 = $c1->get_result();
+        
+        if ($result->num_rows == 0) {
+          if ($result1->num_rows == 0) {
+            $q = $database->prepare("INSERT INTO utilisateur (username, mdp, mail, nom, prenom) VALUES (?, ?, ?, ?, ?)");
+
+            if ($q === false) {
+                die("Erreur de préparation de la requête: " . $database->error);
+            }
+
+            $q->bind_param("sssss", $_POST['username'], $_POST['password'], $_POST['email'], $_POST['nom'], $_POST['prenom']);
+            
+            if ($q->execute()) {
+                echo "Le compte a été créé";
+                // Redirection vers la page "connexion.php"
+                header("Location: connexion.php");
+                exit(); // Assurez-vous de terminer le script après la redirection
+            } else {
+                echo "Erreur lors de l'enregistrement: " . $q->error;
+            }
+
+            $q->close();
+        } else {
+            echo "Un compte avec cet email existe déjà!";
+        }
+    }
+
+        else {
+            echo "Un compte avec ce nom d'utilisateur existe déjà!";
+        }
+        
+  }$c->close();
+  $c1->close();
+}
+
+?>
+
+
 <body>
+
   <div class="container">
     <h1>Inscription</h1>
-    <form name="loginForm" onsubmit="return validateForm()" action="connexion.php" method="POST">
+    <form name="loginForm" onsubmit="return validateForm()" action="inscription.php" method="post">
       <label for="nom">Nom :</label>
       <input type="text" id="nom" name="nom" placeholder="Martin" required>
       <label for="prenom"> Prénom : </label>
@@ -31,14 +85,11 @@
       <input type="password" id="password" name="password" placeholder="motdepasse"required>
       <label for="password"> Confirmer mot de passe : </label>
       <input type="password" id="password2" name="password2" placeholder="motdepasse" required>
-      <input type="submit" value="S'inscrire">
+      <input type="submit" name="button1" value="S'inscrire">
       <input type="button" value="Annuler" onclick="window.location.href = 'connexion.php'">
     </form>
 
-    <?php
-        if(isset($_POST['formsend']))
 
-    ?>
 
   </div>
   <div id="logo">
